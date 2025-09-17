5e8c1f05775ba835240b59c9300e5fe402af993d
Mikołaj <kaleta222@gmail.com>
2025-09-17 10:10:36 +0200

    refactor(constants): update API_URL to point to the correct backend

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 // export const API_URL = 'http://localhost:3000'
-export const API_URL = 'https://zrdzieci.diecezja.pl'
+export const API_URL = 'https://rosary-backend.onrender.com'
+// export const API_URL = 'https://zrdzieci.diecezja.pl'
